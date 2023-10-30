20871e9aada4142f0f9728b0d197a7e903bc619c
Giorgio Liggio <liggiorgio@live.com>
2023-10-30 20:43:41 +0100

    Implement IV pages for news

--- telegrapher.py
+++ telegrapher.py
@@ -4,7 +4,40 @@ from bs4 import BeautifulSoup
 from telegraph import Telegraph
 
 
-# Create a new Telegraph URL from a job listing URL
+URL_HOME = 'https://www.unimi.it'
+ACCOUNT_NEWS = 'avvisiunimi'
+ACCOUNT_JOBS = 'collaborazioniunimi'
+ICON_FILE = '📄'
+ICON_LINK = '🔗'
+
+
+def create_news_IV_page(url: str):
+    job_res = req.get(url)
+    source = job_res.text
+    soup = BeautifulSoup(source, 'lxml')
+    
+    header = soup.find('h1', {'class': 'page-header'}).span.text
+    temp = soup.find('div', {'class': 'col-sm-7 col-md-8 bs-region bs-region--top-left'})
+    for span in temp.find_all('span'):
+        span.unwrap()
+    for div in temp.find_all('div'):
+        div.unwrap()
+    links = soup.find('div', {'class': 'elenco-allegati'})
+    if links:
+        attachments = soup.new_tag('blockquote')
+        for i, a in enumerate(links.find_all('a')):
+            entry = soup.new_tag('li')
+            entry.string = f'{ICON_FILE} ' if a.has_attr('type') else f'{ICON_LINK} '
+            entry.append(a)
+            if i:
+                attachments.append(soup.new_tag('br'))
+            attachments.append(entry)
+        temp.append(attachments)
+    contents = temp.decode_contents().strip()
+
+    return publish_iv(ACCOUNT_NEWS, header, contents)
+
+
 def create_job_IV_page(job_url: str):
     job_res = req.get(job_url)
     source = job_res.text
@@ -13,24 +46,27 @@ def create_job_IV_page(job_url: str):
     header = soup.find('h1', {'class': 'page-header'}).span.text
     author = soup.find('div', {'class': 'icon building'}).a.text
     author = f'{author[:125]}...' if len(author) > 128 else author
-    author_link = 'https://www.unimi.it' + soup.find('div', {'class': 'icon building'}).a['href']
-    clean_tags(soup)
+    author_link = f'{URL_HOME}{soup.find("div", {"class": "icon building"}).a["href"]}'
+    clean_tags_jobs(soup)
     temp = soup.find('div', {'class': 'row bs-2col-bricked node node--type-opportunita node--view-mode-full'})
     description = soup.new_tag('p')
     temp.wrap(description)
     temp.unwrap()
 
+    return publish_iv(ACCOUNT_JOBS, header, description.prettify(), author, author_link)
+
+
+def publish_iv(account, header, content, author=None, author_link=None):
     tg = Telegraph()
-    tg.create_account(short_name = 'collaborazioniunimi')
+    tg.create_account(short_name = account)
     response = tg.create_page(title = header,
-        html_content = description.prettify(),
+        html_content = content,
         author_name = author,
         author_url = author_link)
     return response['url']
 
 
-# Remove non-parsable tags
-def clean_tags(soup):
+def clean_tags_jobs(soup):
     soup.find('div', {'class': 'col-sm-12 bs-region bs-region--top'}).extract() #empty
     soup.find('div', {'class': 'col-sm-6 col-md-8 bs-region bs-region--top-left'}).unwrap() #body
     soup.find('div', {'class': 'bottom10 top10 bp-text'}).unwrap() #description
